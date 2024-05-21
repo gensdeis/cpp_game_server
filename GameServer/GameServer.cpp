dee01651523dba0e2b670a30beeeed487d5557f5
KimHamin <gensdeis@wemade.com>
2024-05-21 06:04:28 +0000

    change platform version

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -2,7 +2,19 @@
 #include <iostream>
 #include "CorePch.h"
 
+#include <thread>
+
+void HelloThread()
+{
+	cout << "Hello Thread" << endl;
+}
+
 int main()
 {
-	HelloWorld();
+	std::thread t(HelloThread);
+	
+	cout << "Hello Main" << endl;
+
+	t.join();
+
 }
